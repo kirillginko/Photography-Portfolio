f58248c8ef5355c86fe263c8a604fd257d98b228
Kirill Ginko <kirillginko@gmail.com>
2021-05-15 16:19:57 -0400

    finished burger menu

--- public/app.js
+++ public/app.js
@@ -1,6 +1,7 @@
-const toggleButton = document.getElementsByClassName("toggle-button")[0];
-const navbarLinks = document.getElementsByClassName("navbar-links")[0];
+const toggleButton = document.getElementsByClassName("burger")[0];
+const navbarLinks = document.getElementsByClassName("nav-links")[0];
 
 toggleButton.addEventListener("click", () => {
+  toggleButton.classList.toggle("active");
   navbarLinks.classList.toggle("active");
 });
