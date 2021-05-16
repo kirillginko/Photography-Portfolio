2137d0d09175fe25af377b3a7b4e4368bd8cc81a
Kirill Ginko <kirillginko@gmail.com>
2021-05-16 19:53:11 -0400

    finished portfolio website

--- public/app.js
+++ public/app.js
@@ -1,7 +1,15 @@
 const toggleButton = document.getElementsByClassName("burger")[0];
 const navbarLinks = document.getElementsByClassName("nav-links")[0];
+const links = navbarLinks.querySelectorAll("a");
 
 toggleButton.addEventListener("click", () => {
   toggleButton.classList.toggle("active");
   navbarLinks.classList.toggle("active");
+});
+
+links.forEach((link) => {
+  link.addEventListener("click", () => {
+    navbarLinks.classList.toggle("active");
+    toggleButton.classList.toggle("active");
+  });
 });
